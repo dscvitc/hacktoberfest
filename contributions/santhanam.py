521a1826bcd7592952c529aad0780c8601459b64
porridgewithraisins <72668511+porridgewithraisins@users.noreply.github.com>
2020-10-11 17:22:52 +0530

    Update santhanam.py

--- contributions/santhanam.py
+++ contributions/santhanam.py
@@ -1,3 +1,1 @@
-Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> print('hello world')
